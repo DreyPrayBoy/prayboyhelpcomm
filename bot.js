82992cd203ee5fc061f12c8193c7062cf969b2a7
DreyPrayBoy <61603994+DreyPrayBoy@users.noreply.github.com>
2020-03-02 18:17:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const ms = require('ms');
+
+const token = 'NjgzMjI3NDU5OTEwNjMxNDM0.Xlofpg.2whKYV5IpV7HYI7izKPlZp02f48';
+
+const PREFIX = '!';
+
+bot.login(process.env.BOT_TOKEN);
